a2c63c1bfca3751a89b504160ff8d71786cfbb71
Trofimov Igor <mister.larpy@ya.ru>
2016-05-25 17:40:29 +0500

    feat(secode): Write api

--- secode/urls.py
+++ secode/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url
 
+from secode import views
 
 urlpatterns = [
+    url(r'^create-list$', views.create_list, name='create-list'),
+    url(r'^delete-list$', views.delete_list, name='delete-list'),
+    url(r'^create-check$', views.add_check_to_list, name='create-check'),
+    url(r'^delete-check$', views.delete_check_from_list, name='delete-check'),
+    url(r'^add-code$', views.add_and_check_code, name='add-code'),
 ]
--- secode/views.py
+++ secode/views.py
@@ -1,3 +1,98 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+from secode.models import CodeCheckList, CodeCheck, Code
+
+
+def create_list(request):
+    if request.method == 'POST':
+        try:
+            title = request.POST['title']
+            ccl = CodeCheckList(title=title)
+            ccl.save()
+            answer = {
+                'status': 'OK',
+                'object': dict(title=ccl.title, id=ccl.id, date=ccl.created)
+            }
+            return JsonResponse(answer)
+        except KeyError:
+            pass
+    answer = {
+        'status': 'FAIL'
+    }
+    return JsonResponse(answer)
+
+
+def delete_list(request):
+    if request.method == 'POST':
+        try:
+            CodeCheckList.objects.filter(pk=request.POST['id']).delete()
+            answer = {
+                'status': 'OK'
+            }
+            return JsonResponse(answer)
+        except KeyError:
+            pass
+    answer = {
+        'status': 'FAIL'
+    }
+    return JsonResponse(answer)
+
+
+def add_check_to_list(request):
+    if request.method == 'POST':
+        try:
+            lista = CodeCheckList.objects.filter(pk=request.POST['id'])
+            title = request.POST['title']
+            ttl = request.POST['ttl']
+            attribute = request.POST['attr']
+            check_regex = request.POST['regex'].encode()
+            cc = CodeCheck(codechecklist=lista, title=title, ttl=ttl, attribute=attribute, check_regex=check_regex)
+            cc.save()
+            answer = {
+                'status': 'OK',
+                'object': {'list_id': request.POST['id'], 'check_id': cc.id, 'title': title, 'ttl': ttl,
+                           'attribute': attribute, 'regex': check_regex.decode()}
+            }
+        except KeyError:
+            pass
+    answer = {
+        'status': 'FAIL'
+    }
+    return JsonResponse(answer)
+
+
+def delete_check_from_list(request):
+    if request.method == 'POST':
+        try:
+            CodeCheckList.objects.filter(pk=request.POST['list_id']).checks.filter(pk=request.POST['check_id']).delete()
+            answer = {
+                'status': 'OK'
+            }
+            return JsonResponse(answer)
+        except KeyError:
+            pass
+    answer = {
+        'status': 'FAIL'
+    }
+    return JsonResponse(answer)
+
+
+def add_and_check_code(request):
+    if request.method == 'POST':
+        try:
+            ids = request.POST['id']
+            code = request.POST['code']
+            c = Code(codechecklist=CodeCheckList.objects.filter(pk=ids), code=code)
+            c.save()
+            answer = {
+                'status': 'OK',
+                'object': {'list_id': request.POST['id'], 'code_id': c.id, 'error': c.error, 'check': c.check_code()}
+            }
+            return JsonResponse(answer)
+        except KeyError:
+            pass
+    answer = {
+        'status': 'FAIL'
+    }
+    return JsonResponse(answer)
